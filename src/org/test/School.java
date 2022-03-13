57f42865b98a09d242b1fa849f203a2cdbde12de
shyam0205 <vigneshshyam8@gmail.com>
2022-03-13 19:05:02 +0530

    new class file

---
+++ src/org/test/School.java
@@ -0,0 +1,40 @@
+package org.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.plaf.FileChooserUI;
+
+public class School {
+	public static void main(String[] args) throws IOException {
+		File k = new File("F:\\vicky\\java\\priya\\sri\\hema\\java.xl");
+		
+//		boolean mkdir = k.mkdir();
+//		System.out.println(mkdir);
+//		
+//		boolean mkdirs = k.mkdirs();
+//		System.out.println(mkdirs);
+//		
+//		boolean file = k.isFile();
+//		System.out.println(file);
+		
+//		boolean directory = k.isDirectory();
+//		System.out.println(directory);
+		
+//		boolean createNewFile = k.createNewFile();
+//		System.out.println(createNewFile);
+		
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
